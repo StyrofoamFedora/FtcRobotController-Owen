d3b54230377d0ff0e0fd4e54a494b3d4608ca3ef
Carter Rommelfanger <github@cookiejar499.me>
2026-01-21 21:57:27 -0600

    MeepMeep now has like, All of the bots

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,10 +1,14 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+//import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.TranslationalVelConstraint;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueLight;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedLight;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
@@ -12,29 +16,80 @@ public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(700);
 
-        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+        RoadRunnerBotEntity BlueCloseBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 16)
+                .setColorScheme(new ColorSchemeBlueDark())
+                .build();
+        RoadRunnerBotEntity RedCloseBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 16)
+                .setColorScheme(new ColorSchemeRedDark())
+                .build();
+        RoadRunnerBotEntity BlueFarBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 16)
+                .setColorScheme(new ColorSchemeBlueLight())
+                .build();
+        RoadRunnerBotEntity RedFarBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 16)
+                .setColorScheme(new ColorSchemeRedLight())
                 .build();
 
         //BLUE CLOSE
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-55, -45, Math.toRadians(229)))
+        BlueCloseBot.runAction(BlueCloseBot.getDrive().actionBuilder(new Pose2d(-56, -46, Math.toRadians(233.5)))
                 .waitSeconds(1)
-                .strafeToLinearHeading(new Vector2d(-30,-20),Math.toRadians(229))
+                .strafeToLinearHeading(new Vector2d(-30,-20),Math.toRadians(233.5))
                 .waitSeconds(5)
-                .strafeToLinearHeading(new Vector2d(-12,-20),Math.toRadians(270))
-                .strafeTo(new Vector2d(-12,-30))
-                .strafeTo(new Vector2d(-12,-52), new TranslationalVelConstraint(4))
+                .strafeToLinearHeading(new Vector2d(-11,-20),Math.toRadians(270))
+                .strafeTo(new Vector2d(-11,-30))
+                .strafeTo(new Vector2d(-11,-52), new TranslationalVelConstraint(4))
                 .waitSeconds(0.5)
-                .strafeToLinearHeading(new Vector2d(-30,-20), Math.toRadians(230))
+                .strafeToLinearHeading(new Vector2d(-30,-20), Math.toRadians(233.5))
                 .waitSeconds(5)
-                        .strafeTo(new Vector2d(0,-30))
+                .strafeTo(new Vector2d(0,-40))
+                .build());
+        //RED CLOSE
+        RedCloseBot.runAction(RedCloseBot.getDrive().actionBuilder(new Pose2d(-56, 46, Math.toRadians(130)))
+                .waitSeconds(1)
+                .strafeToLinearHeading(new Vector2d(-30,20),Math.toRadians(130))
+                .waitSeconds(5)
+                .strafeToLinearHeading(new Vector2d(-11,20),Math.toRadians(90))
+                .strafeTo(new Vector2d(-11,30))
+                .strafeTo(new Vector2d(-11,52), new TranslationalVelConstraint(4))
+                .waitSeconds(0.5)
+                .strafeToLinearHeading(new Vector2d(-30,20), Math.toRadians(130))
+                .waitSeconds(5)
+                .strafeTo(new Vector2d(0,40))
                 .build());
 
+        BlueFarBot.runAction(BlueFarBot.getDrive().actionBuilder(new Pose2d(60, -16, Math.toRadians(210)))
+                .waitSeconds(6)
+                .strafeToLinearHeading(new Vector2d(36,-20),Math.toRadians(270))
+                .strafeTo(new Vector2d(36,-30))
+                .strafeTo(new Vector2d(36,-52), new TranslationalVelConstraint(4))
+                .strafeToLinearHeading(new Vector2d(55,-16), Math.toRadians(210))
+                .waitSeconds(5)
+                .strafeTo(new Vector2d(36,-36))
+                .build());
+
+        RedFarBot.runAction(RedFarBot.getDrive().actionBuilder(new Pose2d(60, 16, Math.toRadians(150)))
+                .waitSeconds(6)
+                .strafeToLinearHeading(new Vector2d(36,20),Math.toRadians(90))
+                .strafeTo(new Vector2d(36,30))
+                .strafeTo(new Vector2d(36,52), new TranslationalVelConstraint(4))
+                .strafeToLinearHeading(new Vector2d(55,16), Math.toRadians(150))
+                .waitSeconds(5)
+                .strafeTo(new Vector2d(36,36))
+                .build());
         meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_JUICE_BLACK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
-                .addEntity(myBot)
+                .addEntity(BlueCloseBot)
+                .addEntity(RedCloseBot)
+                .addEntity(BlueFarBot)
+                .addEntity(RedFarBot)
                 .start();
     }
 }
