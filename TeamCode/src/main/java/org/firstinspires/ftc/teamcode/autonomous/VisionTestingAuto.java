d14d6c5cae52ec50bf0762e9d372cf54f4d8ba5d
Carter Rommelfanger <github@cookiejar499.me>
2026-01-24 21:24:41 -0600

    This Jawn Sorts now. Heck Yeah

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/VisionTestingAuto.java
@@ -0,0 +1,381 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+//Imports
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.ParallelAction;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.TranslationalVelConstraint;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Class Define
+@Config
+@Autonomous(name = "AUTO_INTAKE_TEST", group = "Autonomous")
+public class VisionTestingAuto extends LinearOpMode {
+    //Create Default Variables for Vision Sets
+    int visionOutputPosition = 0;
+    int apriltagid = 21;
+    int CPR = 538;
+    int slotTicks = CPR/3;
+
+    //Set up Actions
+    public class Eyes {
+
+        public final AprilTagProcessor aprilTag;
+        public final VisionPortal visionPortal;
+
+        public int detectedTag = -1;
+        public AprilTagDetection lastDetection = null;
+
+        public Eyes(HardwareMap hardwareMap) {
+
+            aprilTag = AprilTagProcessor.easyCreateWithDefaults();
+
+            visionPortal = VisionPortal.easyCreateWithDefaults(
+                    hardwareMap.get(WebcamName.class, "eyes"),
+                    aprilTag
+            );
+        }
+
+        /** Poll AprilTag detections */
+        public void update() {
+            List<AprilTagDetection> detections = aprilTag.getDetections();
+
+            if (!detections.isEmpty()) {
+                lastDetection = detections.get(0);
+                detectedTag = lastDetection.id;
+            }
+        }
+
+        /** Optional cleanup */
+        public void stop() {
+            visionPortal.close();
+        }
+    }
+    public class WaitForTagAction implements Action {
+
+        private final Eyes eyes;
+        private final long timeoutMs;
+        private long startTime = 0;
+
+        public WaitForTagAction(Eyes eyes, long timeoutMs) {
+            this.eyes = eyes;
+            this.timeoutMs = timeoutMs;
+        }
+
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+
+            if (startTime == 0) {
+                startTime = System.currentTimeMillis();
+            }
+
+            eyes.update();
+
+            packet.put("AprilTag ID", eyes.detectedTag);
+
+            boolean timedOut =
+                    System.currentTimeMillis() - startTime >= timeoutMs;
+
+            // keep running while no tag and not timed out
+            return eyes.detectedTag == -1 && !timedOut;
+        }
+    }
+    public class shooter {
+        private final DcMotorEx motor1;
+
+
+
+        public shooter(HardwareMap hardwareMap) {
+            motor1 = hardwareMap.get(DcMotorEx.class, "rightFly");
+
+            PIDFCoefficients pidfCoefficients = new PIDFCoefficients(75,0,0,13.2);
+            motor1.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfCoefficients);
+
+
+        }
+
+
+        public class SpinUp implements Action {
+            private boolean initialized = false;
+
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                if (!initialized) {
+                    motor1.setVelocity(-1200);
+                    initialized = true;
+                }
+
+                return false;
+            }
+
+
+        }
+        public Action spinUp() {
+            return new SpinUp();
+        }
+        public class SpinDown implements Action {
+
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                motor1.setVelocity(0);
+                return false;
+            }
+        }
+        public Action spinDown(){
+            return new SpinDown();
+        }
+    }
+    public class kick {
+        private Servo kicker;
+
+
+        public kick(HardwareMap hardwareMap) {
+            kicker = hardwareMap.get(Servo.class, "ethan");
+        }
+
+
+        public class kickDown implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                kicker.setPosition(1);
+                sleep(100);
+                return false;
+            }
+        }
+        public Action kickDown() {
+            return new kickDown();
+        }
+
+
+        public class kickUp implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                kicker.setPosition(0);
+                sleep(100);
+                return false;
+            }
+        }
+        public Action kickUp() {
+            return new kickUp();
+        }
+    }
+    public class combine {
+        private final DcMotorEx intake;
+
+        public combine(HardwareMap hardwareMap) {
+            intake = hardwareMap.get(DcMotorEx.class, "bottomIntake");
+            intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        }
+        public class Intake implements Action {
+            private boolean initialized = false;
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                if (!initialized) {
+                    intake.setPower(0.5);
+                    initialized = true;
+                    sleep(500);
+            }
+                return false;
+            }
+
+
+        }
+        public Action intake() {
+            return new Intake();
+        }
+
+
+        public class Outtake implements Action {
+            private boolean initialized = false;
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                if (!initialized) {
+                    intake.setPower(-0.5);
+                    initialized = true;
+                }
+                return false;
+            }
+
+
+        }
+        public Action outtake() {
+            return new Outtake();
+        }
+
+
+        public class Holdtake implements Action {
+
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                intake.setPower(0);
+                return false;
+            }
+        }
+        public Action holdtake(){
+            return new Holdtake();
+        }
+    }
+    public class spindex {
+        private final DcMotorEx spindexer;
+        private final DistanceSensor frontDistance;
+        public spindex(HardwareMap hardwareMap) {
+            spindexer = hardwareMap.get(DcMotorEx.class, "topIntake");
+            spindexer.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            frontDistance = hardwareMap.get(DistanceSensor.class,"frontSlot");
+        }
+        public class NextSlot implements Action {
+            private boolean initialized = false;
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                if (!initialized) {
+                    spindexer.setTargetPosition(spindexer.getCurrentPosition()+slotTicks);
+                    spindexer.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+                    spindexer.setPower(.3);
+                    initialized = true;
+                    sleep(400);
+                }
+                return false;
+            }
+        }
+        public Action nextSlot() {
+            return new NextSlot();
+        }
+        public class PrevSlot implements Action {
+            private boolean initialized = false;
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                if (!initialized) {
+                    spindexer.setTargetPosition(spindexer.getCurrentPosition()-slotTicks);
+                    spindexer.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+                    spindexer.setPower(.3);
+                    initialized = true;
+                }
+                return false;
+            }
+        }
+        public Action prevSlot() {
+            return new PrevSlot();
+        }
+        public class AutoIntake implements Action {
+            private boolean initialized = false;
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                if (!initialized) {
+
+                    initialized = true;
+                }
+                if (frontDistance.getDistance(DistanceUnit.CM)>8){
+                    return true;
+                }
+                else{return false;}
+            }
+        }
+        public Action autoIntake() {
+            return new AutoIntake();
+        }
+    }
+
+    //Set up Classes for Trajectory + Actions
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Pose2d initialPose = new Pose2d(-56, -46, Math.toRadians(235.5));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
+        shooter shooter = new shooter(hardwareMap);
+        kick kick = new kick(hardwareMap);
+        combine combine = new combine(hardwareMap);
+        spindex spindex = new spindex(hardwareMap);
+        Eyes eyes = new Eyes(hardwareMap);
+        while (!isStopRequested() && !opModeIsActive()) {
+            int position = visionOutputPosition;
+            telemetry.addData("Position during Init", apriltagid);
+            telemetry.update();
+            if (isStopRequested()) return;
+        }
+
+
+        int startPosition = visionOutputPosition;
+        telemetry.addData("Starting Position", startPosition);
+        telemetry.update();
+        waitForStart();
+// Trajectories
+        TrajectoryActionBuilder visionSet = drive.actionBuilder(initialPose)
+                .strafeToLinearHeading(new Vector2d(-30,-20),Math.toRadians(180));
+        TrajectoryActionBuilder shootSet1 = drive.actionBuilder(new Pose2d(-30,-20,Math.toRadians(180)))
+                .strafeToLinearHeading(new Vector2d(-30,-20),Math.toRadians(231.5));
+        TrajectoryActionBuilder intakeTopSet = drive.actionBuilder(new Pose2d(-30,-20, Math.toRadians(231.5)))
+                .strafeToLinearHeading(new Vector2d(-11,-20),Math.toRadians(270));
+        TrajectoryActionBuilder intakingTop = drive.actionBuilder(new Pose2d(-11,-20, Math.toRadians(270)))
+                .strafeTo(new Vector2d(-11,-30))
+                .strafeTo(new Vector2d(-11,-52), new TranslationalVelConstraint(4));
+        TrajectoryActionBuilder outsideSet = drive.actionBuilder(new Pose2d(-30,-20,Math.toRadians(231.5)))
+                .strafeTo(new Vector2d(0,-40));
+        TrajectoryActionBuilder shootSet2 = drive.actionBuilder(new Pose2d(-11,-52,Math.toRadians(270)))
+                .strafeToLinearHeading(new Vector2d(-30,-20), Math.toRadians(231.5));
+
+        Action waitForTag = new WaitForTagAction(eyes,1500);
+        Action unloadBalls =  new SequentialAction(
+                kick.kickUp(), new SleepAction(0.2),
+                kick.kickDown(), new SleepAction(0.5),
+                spindex.nextSlot(), new SleepAction(1),
+                kick.kickUp(), new SleepAction(0.2),
+                kick.kickDown(), new SleepAction(1),
+                spindex.nextSlot(), new SleepAction(1),
+                kick.kickUp(), new SleepAction(0.2),
+                kick.kickDown());
+
+//Stuff That's run
+        Actions.runBlocking(new SequentialAction(
+                visionSet.build(),
+                waitForTag
+        ));
+        Action ballOrganize;
+        if (eyes.detectedTag==21){
+            ballOrganize = spindex.nextSlot();
+        } else if (eyes.detectedTag==23) {
+            ballOrganize = spindex.prevSlot();
+        } else  {
+            ballOrganize = new SleepAction(1);
+        }
+        eyes.stop();
+        Actions.runBlocking(new SequentialAction(
+                new SleepAction(1),
+                new ParallelAction(
+                    ballOrganize,
+                    shootSet1.build(),
+                    shooter.spinUp()
+                ),
+                unloadBalls,
+                shooter.spinDown()
+        ));
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tele/VisionFieldCentricMecanumTeleOp.java
@@ -0,0 +1,224 @@
+package org.firstinspires.ftc.teamcode.tele;
+
+import android.util.Size;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorRangeSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.JavaUtil;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@TeleOp
+public class VisionFieldCentricMecanumTeleOp extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        // Make sure your ID's match your configuration
+         DcMotor backLeftMotor = hardwareMap.get(DcMotor.class,"left_back_drive");
+         DcMotor frontRightMotor = hardwareMap.get(DcMotor.class, "right_front_drive");
+         DcMotor backRightMotor = hardwareMap.get(DcMotor.class,"right_back_drive");
+         DcMotor frontLeftMotor = hardwareMap.get(DcMotor.class,"left_front_drive");
+         DcMotorEx rightFly = hardwareMap.get(DcMotorEx.class,"rightFly");
+         DcMotorEx spindexer = hardwareMap.get(DcMotorEx.class,"topIntake");
+         DcMotor Intake = hardwareMap.get(DcMotor.class, "bottomIntake");
+         Servo kick = hardwareMap.servo.get("ethan");
+         Servo RGB = hardwareMap.servo.get("rgb");
+         WebcamName eyes = hardwareMap.get(WebcamName.class,"eyes");
+         ColorRangeSensor shootColor = hardwareMap.get(ColorRangeSensor.class, "color");
+        DistanceSensor frontDistance = hardwareMap.get(DistanceSensor.class,"frontSlot");
+        rightFly.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        spindexer.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        spindexer.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+        spindexer.setTargetPositionTolerance(2);
+        spindexer.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        spindexer.setTargetPosition(0);
+        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        AprilTagProcessor aprilTagProcessor;
+        VisionPortal visionPortal;
+        List<AprilTagDetection> detectedTags = new ArrayList<>();
+        aprilTagProcessor = new AprilTagProcessor.Builder().setDrawTagID(true).build();
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+        builder.setCamera(hardwareMap.get(WebcamName.class,"eyes"));
+        builder.setCameraResolution(new Size(640,480));
+        builder.addProcessor(aprilTagProcessor);
+        visionPortal = builder.build();
+
+
+
+        // CHECK IF NEEDED, FOR IF MOTORS ARE BACKWARDS
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        // Retrieve the IMU from the hardware map
+        IMU imu = hardwareMap.get(IMU.class, "imu");
+        // Adjust the orientation parameters to match your robot
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.RIGHT));
+        imu.initialize(parameters);
+
+        //Gamepad Setups for Rising Edge Detectors (just on triggers)
+        Gamepad currentGamepad1 = new Gamepad();
+        Gamepad currentGamepad2 = new Gamepad();
+        Gamepad previousGamepad1 = new Gamepad();
+        Gamepad previousGamepad2 = new Gamepad();
+
+        //Set Values for PID and Intake
+        double targetFlywheelVelo = 0;
+        double intakePower = 0;
+        int ballSlot = 0;
+        double P = 75;
+        double F = 13.2;
+        double CPR = 537.7;
+        double slotTicks = CPR/3;
+        int slotOne = 1;
+        int slotTwo = 1;
+        int slotThree = 2;
+        int currentIntTick;
+        double currentTick = 0;
+        String shootBall;
+
+        PIDFCoefficients pidfCoefficients = new PIDFCoefficients(P,0,0,F);
+        rightFly.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfCoefficients);
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        //Tele-op Loop
+        while (opModeIsActive()) {
+
+            //Set up game pads
+            previousGamepad1.copy(currentGamepad1);
+            previousGamepad2.copy(currentGamepad2);
+            currentGamepad1.copy(gamepad1);
+            currentGamepad2.copy(gamepad2);
+
+            //Take values from sticks
+            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x;
+            double rx = gamepad1.right_stick_x*0.8;
+
+            // This button choice was made so that it is hard to hit on accident,
+            if (gamepad1.options) {
+                imu.resetYaw();
+            }
+
+            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+            // Rotate the movement direction counter to the bot's rotation
+            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+            rotX = rotX * 1.1;  // Counteract imperfect strafing
+
+            // Denominator is the largest motor power (absolute value) or 1
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+            double frontLeftPower = (rotY + rotX + rx) / denominator;
+            double backLeftPower = (rotY - rotX + rx) / denominator;
+            double frontRightPower = (rotY - rotX - rx) / denominator;
+            double backRightPower = (rotY + rotX - rx) / denominator;
+
+            //Match Buttons Pressed GamePad 1 (Driver [non-driving])
+            if (currentGamepad1.right_trigger>0.2 && previousGamepad1.right_trigger<0.2){intakePower = 0.8;}//Intake
+            if (currentGamepad1.left_trigger>0.2 && previousGamepad1.left_trigger<0.2){intakePower=-0.8;}//Outtake
+            if (currentGamepad1.right_trigger<0.2 && previousGamepad1.right_trigger>0.2){intakePower=0;}//Intake Shutoff
+            if (currentGamepad1.left_trigger<0.2 && previousGamepad1.left_trigger>0.2){intakePower=0;}//Outtake Shutoff
+
+            // Match Buttons Pressed Gamepad 2 (Shooter[ball slots])
+            if (gamepad2.dpadLeftWasPressed()){
+                currentTick-=slotTicks;
+                currentIntTick = (int)Math.round(currentTick);
+                spindexer.setTargetPosition(currentIntTick);
+                spindexer.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+                spindexer.setPower(.3);
+                ballSlot -= 1;
+
+            } //Previous Ball Slot
+            if (gamepad2.dpadRightWasPressed()){
+                currentTick+=slotTicks;
+                currentIntTick = (int)Math.round(currentTick);
+                spindexer.setTargetPosition(currentIntTick);
+                spindexer.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+                spindexer.setPower(.3);
+                ballSlot += 1;
+            } //Next Ball Slot
+            if (gamepad2.yWasPressed()){if(ballSlot == 0){slotOne = 1;}else if(ballSlot == 1){slotTwo = 1;}else if(ballSlot == 2){slotThree = 1;}}//Set Current slot Green
+            if (gamepad2.bWasPressed()){if(ballSlot == 0){slotOne = 2;}else if(ballSlot == 1){slotTwo = 2;}else if(ballSlot == 2){slotThree = 2;}}//Set Current slot Purple
+            //Rest of the Gamepad2 controls {Shooter[shooting]}
+            if (currentGamepad2.right_trigger>0.2 && previousGamepad2.right_trigger<0.2){kick.setPosition(0);}// Shoot
+            if (currentGamepad2.right_trigger<0.2 && previousGamepad2.right_trigger>0.2){kick.setPosition(1);}// UnSHoot
+            if (currentGamepad2.left_trigger>0.2 && previousGamepad2.left_trigger<0.2){targetFlywheelVelo = 1200;}//Spin Up
+            if (gamepad2.leftBumperWasPressed()){ targetFlywheelVelo = 0;} //Spin Down
+            if (gamepad2.dpadUpWasPressed()){targetFlywheelVelo += 20;} //Increase Power
+            if (gamepad2.dpadDownWasPressed()){targetFlywheelVelo -= 20;} //Decrease Power
+          
+            //PID Loop for motor velocity
+            /*if (targetFlywheelVelo == 1200){RGB.setPosition(.5);}//Set LED to Green
+            else{RGB.setPosition(.28);} //Set LED to RED
+*/
+            /*if(frontDistance.getDistance(DistanceUnit.CM)<15){RGB.setPosition(.5);}//Set LED to Green
+            else{RGB.setPosition(.28);} //Set LED to RED*/
+
+            double hue = JavaUtil.colorToHue(shootColor.getNormalizedColors().toColor());
+            if(hue < 50){shootBall = "None"; }
+            else if (hue < 200) {shootBall = "Green"; }
+            else if (hue < 350){shootBall = "Purple"; }
+            else {shootBall = "None"; }
+
+            if(shootBall.equals("Green")){RGB.setPosition(.5);}//Set LED to Green
+            else if (shootBall.equals("Purple")) {RGB.setPosition(.72);}
+            else{RGB.setPosition(.5);}
+
+            /*if (shootColor.getDistance(DistanceUnit.CM)>8 && frontDistance.getDistance(DistanceUnit.CM)<8) {
+                currentTick-=slotTicks;
+                currentIntTick = (int)Math.round(currentTick);
+                spindexer.setTargetPosition(currentIntTick);
+                spindexer.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+                spindexer.setPower(.3);
+                ballSlot -= 1;
+            }*/
+
+            //Run everything
+            frontLeftMotor.setPower(frontLeftPower);
+            backLeftMotor.setPower(backLeftPower);
+            frontRightMotor.setPower(frontRightPower);
+            backRightMotor.setPower(backRightPower);
+            rightFly.setVelocity(-targetFlywheelVelo);
+            Intake.setPower(intakePower);
+            spindexer.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+            spindexer.setPower(.3);
+
+            //Set up
+            ballSlot = Math.abs(ballSlot%3);
+            detectedTags = aprilTagProcessor.getDetections();
+            //telemetry
+            telemetry.addData("Flywheel %",targetFlywheelVelo);
+            telemetry.addData("Current Slot",ballSlot);
+            telemetry.addData("Slot1",slotOne);
+            telemetry.addData("Slot2",slotTwo);
+            telemetry.addData("Slot 3",slotThree);
+            telemetry.addData("Distance",frontDistance.getDistance(DistanceUnit.CM));
+            telemetry.addData("Color", shootBall);
+            telemetry.update();
+        }
+    }
+}
